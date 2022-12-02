a439efc42b09643efd983317eb7c502ca4b9e4c7
shagie <turnmichael@gmail.com>
2022-12-01 23:53:14 -0600

    feat: Day 2 part two

--- src/main/java/net/shagie/aoc/twentytwentytwo/day2/DayTwoRockPaperScissors.java
+++ src/main/java/net/shagie/aoc/twentytwentytwo/day2/DayTwoRockPaperScissors.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component("day2rps")
 @AOC(day = 2)
@@ -31,6 +33,53 @@ public class DayTwoRockPaperScissors extends AbstractAOCDay {
                 .map(s -> s.split(" "))
                 .map(a -> new ERound(lookup.get(a[0]), lookup.get(a[1])))
                 .mapToInt(er -> er.u.score + Rps.beats(er.t, er.u).score)
+                .sum();
+        System.out.println(score);
+    }
+
+    // The Elf finishes helping with the tent and sneaks back over to you.
+    // "Anyway, the second column says how the round needs to end:
+    //   X means you need to lose,
+    //   Y means you need to end the round in a draw, and
+    //   Z means you need to win.
+    //   Good luck!"
+    @Override
+    public void partTwo() {
+        Map<Rps, Function<String, Rps>> flookup = new EnumMap<>(Rps.class);
+
+        flookup.put(Rps.PAPER, s -> switch (s) {
+            case "X" -> Rps.ROCK;
+            case "Y" -> Rps.PAPER;
+            case "Z" -> Rps.SCISSORS;
+            default -> Rps.ROCK;
+        });
+        flookup.put(Rps.ROCK, s -> switch (s) {
+            case "X" -> Rps.SCISSORS;
+            case "Y" -> Rps.ROCK;
+            case "Z" -> Rps.PAPER;
+            default -> Rps.ROCK;
+        });
+        flookup.put(Rps.SCISSORS, s -> switch (s) {
+            case "X" -> Rps.PAPER;
+            case "Y" -> Rps.SCISSORS;
+            case "Z" -> Rps.ROCK;
+            default -> Rps.ROCK;
+        });
+
+        Map<String, Rps> rlookup = new HashMap<>();
+        for (Rps value : Rps.values()) {
+            rlookup.put(value.they, value);
+        }
+
+        record Round(Rps them, Rps us) {
+        }
+
+        int score = Arrays.stream(txt.split("\n"))
+                .map(l -> l.split(" "))
+                .map(a -> new Round(rlookup.get(a[0]),
+                        flookup.get(rlookup.get(a[0])).apply(a[1])
+                ))
+                .mapToInt(er -> er.us.score + Rps.beats(er.them, er.us).score)
                 .sum();
         System.out.println(score);
     }
