2603ff8e33ca71f0585cf043c0ff0b0dc3d77cf0
shagie <turnmichael@gmail.com>
2022-12-03 00:12:46 -0600

    feat: Day 3 part 2

--- src/main/java/net/shagie/aoc/twentytwentytwo/day3/Day3Rucksack.java
+++ src/main/java/net/shagie/aoc/twentytwentytwo/day3/Day3Rucksack.java
@@ -1,19 +1,17 @@
 package net.shagie.aoc.twentytwentytwo.day3;
 
 import net.shagie.aoc.twentytwentytwo.util.AOC;
-import net.shagie.aoc.twentytwentytwo.util.AbstractAOCDay;
+import net.shagie.aoc.twentytwentytwo.util.AOCDay;
+import org.apache.commons.collections4.ListUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @AOC(day = 3)
 @Component("day3sack")
 @SuppressWarnings("java:S106")  // System.out.println is fine
-public class Day3Rucksack extends AbstractAOCDay {
+public class Day3Rucksack implements AOCDay {
     @Value("#{resourceReader.readFileToLoS('classpath:day3/data.txt')}")
     List<String> txt;
 
@@ -38,6 +36,31 @@ public class Day3Rucksack extends AbstractAOCDay {
                     return data.iterator().next().charAt(0);
                 })
                 .mapToInt(c -> (c >= 'a') ? (c - 'a' + 1) : 26 + (c - 'A' + 1))
+                .sum();
+        System.out.println(answer);
+    }
+
+    @Override
+    public void partTwo() {
+        List<List<String>> groups = ListUtils.partition(txt, 3);
+        int answer = groups.stream()
+                .mapToInt(los -> {
+                    Map<Character, Integer> counts = new LinkedHashMap<>();
+                    los.stream()
+                            .map(s -> Arrays.stream(s.split(""))
+                                    .distinct()
+                                    .map(st -> st.charAt(0))
+                                    .toList()
+                            )
+                            .forEach(loc -> loc.forEach(c ->
+                                    counts.put(c, counts.getOrDefault(c, 0) + 1)
+                            ));
+                    char key = counts.entrySet().stream().filter(entry -> entry.getValue() == 3)
+                            .findFirst()
+                            .map(Map.Entry::getKey)
+                            .orElse(' ');
+                    return (key >= 'a') ? (key - 'a' + 1) : 26 + (key - 'A' + 1);
+                })
                 .sum();
         System.out.println(answer);
     }
