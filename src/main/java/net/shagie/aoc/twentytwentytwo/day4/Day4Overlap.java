d3608767921afb477e1d51e7927ce4a2f909c3fb
shagie <turnmichael@gmail.com>
2022-12-03 23:20:33 -0600

    feat: Day 4 part 2

--- src/main/java/net/shagie/aoc/twentytwentytwo/day4/Day4Overlap.java
+++ src/main/java/net/shagie/aoc/twentytwentytwo/day4/Day4Overlap.java
@@ -50,6 +50,28 @@ public class Day4Overlap implements AOCDay {
 
     @Override
     public void partTwo() {
-        System.out.println("part 2");
+        int count = 0;
+
+        for (String line : txt) {
+            String[] segments = line.split(",");
+            int zs = Integer.parseInt(segments[0].split("-")[0]);
+            int ze = Integer.parseInt(segments[0].split("-")[1]);
+            Set<Integer> zero = new HashSet<>();
+            for (int i = zs; i <= ze; i++) {
+                zero.add(i);
+            }
+
+            int os = Integer.parseInt(segments[1].split("-")[0]);
+            int oe = Integer.parseInt(segments[1].split("-")[1]);
+            Set<Integer> one = new HashSet<>();
+            for (int i = os; i <= oe; i++) {
+                one.add(i);
+            }
+
+            Set<Integer> zro = new HashSet(zero);
+            zro.retainAll(one);
+            count += zro.size() > 0 ? 1 : 0;
+        }
+        System.out.println(count);
     }
 }
