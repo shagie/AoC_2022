b74ec752e261e8c116bcded99a002115af90f82b
shagie <turnmichael@gmail.com>
2022-12-04 23:51:17 -0600

    feat: Day 5 part 2

--- src/main/java/net/shagie/aoc/twentytwentytwo/day5/Day5Stacks.java
+++ src/main/java/net/shagie/aoc/twentytwentytwo/day5/Day5Stacks.java
@@ -16,8 +16,8 @@ public class Day5Stacks implements AOCDay {
 
     @Override
     public void partOne() {
-        List<String> init = txt.get(0);
-        List<String> moves = txt.get(1);
+        List<String> init = new ArrayList<>(txt.get(0));
+        List<String> moves = new ArrayList<>(txt.get(1));
         String[] sc = init.get(init.size() - 1).trim().split("\s+");
         int stackCount = sc.length;
 
@@ -71,6 +71,60 @@ public class Day5Stacks implements AOCDay {
 
     @Override
     public void partTwo() {
+        List<String> init = new ArrayList<>(txt.get(0));
+        List<String> moves = new ArrayList<>(txt.get(1));
+        String[] sc = init.get(init.size() - 1).trim().split("\s+");
+        int stackCount = sc.length;
 
+        // [V] [G] [C] [Q] [T] [J] [P] [B] [M]
+        //  1   2   3   4   5   6   7   8   9
+
+        List<Deque<Character>> stacks = new ArrayList<>(stackCount);
+        for (int i = 0; i < stackCount; i++) {
+            stacks.add(i, new LinkedList<>());
+        }
+
+        Collections.reverse(init);
+        Deque<String> ll = new LinkedList(init);
+        ll.removeFirst();
+
+        for (String line : ll) {
+            for (int pos = 0; pos < stackCount; pos++) {
+                int col = 1 + pos * 4;
+                if (line.length() >= col) {
+                    Character p = line.charAt(1 + pos * 4);
+                    System.out.println(pos + " " + p);
+                    if (!p.equals(' ')) {
+                        stacks.get(pos).push(p);
+                    }
+                }
+            }
+            System.out.println();
+            System.out.println();
+        }
+        System.out.println("Now do moves...");
+
+        for (String line : moves) {
+            String[] parts = line.split(" ");
+            int count = Integer.parseInt(parts[1]);
+            int from = Integer.parseInt(parts[3]);
+            int to = Integer.parseInt(parts[5]);
+
+            var fromStack = stacks.get(from - 1);
+            var toStack = stacks.get(to - 1);
+
+            var holding = new LinkedList<Character>();
+            for (int c = 0; c < count; c++) {
+                holding.push(fromStack.pop());
+            }
+            for (int c = 0; c < count; c++) {
+                toStack.push(holding.pop());
+            }
+        }
+
+        for (var stack : stacks) {
+            System.out.print(stack.peekFirst());
+        }
+        System.out.println();
     }
 }
